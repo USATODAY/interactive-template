eeaf4b9b42de5bc036f2757d71b75da1bcdfe62b
mitchthorson <mthorson@usatoday.com>
2015-07-07 15:04:41 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-ln -sf $PWD ~/.grunt-init
+ln -sf $PWD ~/.grunt-init/interactive-template
 echo "You're setup and ready to go. Run grunt-init interactive-template in an empty directory to start a new project!"
